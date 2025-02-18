add64ab39940bf62973bd6291eec91e8a07e68e2
take0913 <tphonex913@outlook.jp>
2025-02-18 20:36:33 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # ↑↑上の記述は削除しましょう↑↑
   root to: "messages#index"
-  devise_for :users
+  resources :users, only: [:edit, :update]
 end
 
